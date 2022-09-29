1021fc6be403d90a705de3c31772e4ff7ee9e619
AdamAbd <adamabd14113@gmail.com>
2022-09-29 18:16:47 +0700

    remove icon button

--- lib/feature/common/presentation/widgets/button.dart
+++ lib/feature/common/presentation/widgets/button.dart
@@ -4,116 +4,116 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spatu_flutter/feature/feature.dart';
 
-class ButtonIcon extends StatelessWidget {
-  const ButtonIcon({
-    Key? key,
-    required VoidCallback onTap,
-    required Widget child,
-    Color backgroundColor = Colors.transparent,
-    bool isDarkSplash = false,
-    bool isCircle = false,
-    Color? splashColor,
-    Color? highlightColor,
-    double? buttonSize,
-    double radius = AppBorderRadius.extraSmall,
-    EdgeInsetsGeometry? padding,
-    double? borderThickness,
-    Color? borderColor,
-  })  : _onTap = onTap,
-        _child = child,
-        _backgroundColor = backgroundColor,
-        _isDarkSplash = isDarkSplash,
-        _splashColor = splashColor,
-        _highlightColor = highlightColor,
-        _buttonSize = buttonSize,
-        _radius = radius,
-        _padding = padding,
-        _borderThickness = borderThickness,
-        _borderColor = borderColor,
-        super(key: key);
+// class ButtonIcon extends StatelessWidget {
+//   const ButtonIcon({
+//     Key? key,
+//     required VoidCallback onTap,
+//     required Widget child,
+//     Color backgroundColor = Colors.transparent,
+//     bool isDarkSplash = false,
+//     bool isCircle = false,
+//     Color? splashColor,
+//     Color? highlightColor,
+//     double? buttonSize,
+//     double radius = AppBorderRadius.extraSmall,
+//     EdgeInsetsGeometry? padding,
+//     double? borderThickness,
+//     Color? borderColor,
+//   })  : _onTap = onTap,
+//         _child = child,
+//         _backgroundColor = backgroundColor,
+//         _isDarkSplash = isDarkSplash,
+//         _splashColor = splashColor,
+//         _highlightColor = highlightColor,
+//         _buttonSize = buttonSize,
+//         _radius = radius,
+//         _padding = padding,
+//         _borderThickness = borderThickness,
+//         _borderColor = borderColor,
+//         super(key: key);
 
-  final VoidCallback _onTap;
-  final Widget _child;
-  final Color _backgroundColor;
-  final bool _isDarkSplash;
-  final Color? _splashColor;
-  final Color? _highlightColor;
-  final double? _buttonSize;
-  final double _radius;
-  final EdgeInsetsGeometry? _padding;
-  final double? _borderThickness;
-  final Color? _borderColor;
+//   final VoidCallback _onTap;
+//   final Widget _child;
+//   final Color _backgroundColor;
+//   final bool _isDarkSplash;
+//   final Color? _splashColor;
+//   final Color? _highlightColor;
+//   final double? _buttonSize;
+//   final double _radius;
+//   final EdgeInsetsGeometry? _padding;
+//   final double? _borderThickness;
+//   final Color? _borderColor;
 
-  @override
-  Widget build(BuildContext context) {
-    final _responsive = ResponsiveUtils(context);
+//   @override
+//   Widget build(BuildContext context) {
+//     final _responsive = ResponsiveUtils(context);
 
-    if (Platform.isAndroid) {
-      return Card(
-        color: _backgroundColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(_radius),
-          side: _borderThickness != null
-              ? BorderSide(
-                  color: _borderColor ?? Colors.white,
-                  width: _borderThickness ?? 1,
-                )
-              : BorderSide.none,
-        ),
-        clipBehavior: Clip.antiAlias,
-        margin: EdgeInsets.zero,
-        elevation: 0,
-        child: InkWell(
-          onTap: _onTap,
-          splashColor:
-              (_splashColor ?? (_isDarkSplash ? Colors.black : Colors.white))
-                  .withOpacity(.2),
-          highlightColor:
-              (_highlightColor ?? (_isDarkSplash ? Colors.black : Colors.white))
-                  .withOpacity(.2),
-          child: Container(
-            constraints: BoxConstraints(
-              minWidth: _responsive
-                  .getResponsiveSize(_buttonSize ?? AppButtonSize.normal),
-              minHeight: _responsive
-                  .getResponsiveSize(_buttonSize ?? AppButtonSize.normal),
-            ),
-            padding: _padding ??
-                (_buttonSize == null
-                    ? const EdgeInsets.all(AppGap.extraSmall)
-                    : EdgeInsets.zero),
-            alignment: Alignment.center,
-            child: _child,
-          ),
-        ),
-      );
-    } else {
-      return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(_radius),
-          border: _borderThickness != null
-              ? Border.all(
-                  color: _borderColor ?? Colors.white,
-                  width: _borderThickness ?? 1,
-                )
-              : null,
-        ),
-        child: CupertinoButton(
-          minSize: AppButtonSize.normal,
-          color: _backgroundColor,
-          disabledColor: _backgroundColor,
-          borderRadius: BorderRadius.circular(_radius),
-          padding: _padding ??
-              (_buttonSize == null
-                  ? const EdgeInsets.all(AppGap.extraSmall)
-                  : EdgeInsets.zero),
-          onPressed: _onTap,
-          child: _child,
-        ),
-      );
-    }
-  }
-}
+//     if (Platform.isAndroid) {
+//       return Card(
+//         color: _backgroundColor,
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(_radius),
+//           side: _borderThickness != null
+//               ? BorderSide(
+//                   color: _borderColor ?? Colors.white,
+//                   width: _borderThickness ?? 1,
+//                 )
+//               : BorderSide.none,
+//         ),
+//         clipBehavior: Clip.antiAlias,
+//         margin: EdgeInsets.zero,
+//         elevation: 0,
+//         child: InkWell(
+//           onTap: _onTap,
+//           splashColor:
+//               (_splashColor ?? (_isDarkSplash ? Colors.black : Colors.white))
+//                   .withOpacity(.2),
+//           highlightColor:
+//               (_highlightColor ?? (_isDarkSplash ? Colors.black : Colors.white))
+//                   .withOpacity(.2),
+//           child: Container(
+//             constraints: BoxConstraints(
+//               minWidth: _responsive
+//                   .getResponsiveSize(_buttonSize ?? AppButtonSize.normal),
+//               minHeight: _responsive
+//                   .getResponsiveSize(_buttonSize ?? AppButtonSize.normal),
+//             ),
+//             padding: _padding ??
+//                 (_buttonSize == null
+//                     ? const EdgeInsets.all(AppGap.extraSmall)
+//                     : EdgeInsets.zero),
+//             alignment: Alignment.center,
+//             child: _child,
+//           ),
+//         ),
+//       );
+//     } else {
+//       return Container(
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(_radius),
+//           border: _borderThickness != null
+//               ? Border.all(
+//                   color: _borderColor ?? Colors.white,
+//                   width: _borderThickness ?? 1,
+//                 )
+//               : null,
+//         ),
+//         child: CupertinoButton(
+//           minSize: AppButtonSize.normal,
+//           color: _backgroundColor,
+//           disabledColor: _backgroundColor,
+//           borderRadius: BorderRadius.circular(_radius),
+//           padding: _padding ??
+//               (_buttonSize == null
+//                   ? const EdgeInsets.all(AppGap.extraSmall)
+//                   : EdgeInsets.zero),
+//           onPressed: _onTap,
+//           child: _child,
+//         ),
+//       );
+//     }
+//   }
+// }
 
 // class ButtonPrimary extends StatelessWidget {
 //   const ButtonPrimary(
